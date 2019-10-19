d8bca411d0e77a6294c9c11df130c399338bc2ce
sanjeev <sanjeevgupta9223@gmail.com>
2019-10-19 18:24:34 -0400

    initialize the extension classes

---
+++ C0761022_F2019_MID_TERM_MAD3004/extensions/MobileMinutees.swift
@@ -0,0 +1,9 @@
+//
+//  MobileMinutees.swift
+//  C0761022_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 com.lambton. All rights reserved.
+//
+
+import Foundation
